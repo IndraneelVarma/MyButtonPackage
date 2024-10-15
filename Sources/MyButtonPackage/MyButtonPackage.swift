845304a57be38a2de7534cd869fdcfb661c94fd0
IndraneelVarma <manthenaindraneel@gmail.com>
2024-10-15 16:09:37 +0530

    my first package

--- Sources/MyButtonPackage/MyButtonPackage.swift
+++ Sources/MyButtonPackage/MyButtonPackage.swift
@@ -1,2 +1,20 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+
+import SwiftUI
+
+struct CustomButton: View {
+    let title: String
+    let color: Color
+    let size: CGFloat
+    let action: () -> Void
+    
+    var body: some View {
+        Button(action: action) {
+            Text(title)
+                .foregroundColor(.white)
+                .padding()
+                .frame(height: size)
+                .background(color)
+                .cornerRadius(size / 4)
+        }
+    }
+}
